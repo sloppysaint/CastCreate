b39eec41df21b6709684d69e17beeafd00b733e5
Piyush2604 <khattrapiyush80@gmail.com>
2024-08-22 23:50:47 +0530

    working

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,9 +1,23 @@
+"use client";
 import PodcastCard from "@/components/PodcastCard";
-import { Button } from "@/components/ui/button";
 import { podcastData } from "@/constants";
 import React from "react";
 
+
+import { useQuery } from "convex/react";
+import { api } from "../convex/_generated/api";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      {tasks?.map(({ _id, text }) => <div key={_id}>{text}</div>)}
+    </main>
+  );
+}
+
+
 const Home = () => {
+  const tasks = useQuery(api.tasks.get);
   return (
     <div className="mt-9 flex flex-col gap-9">
       <section className="flex flex-col gap-5">
