211bf821fe4b6b988a6482adb1d7ce41c39a2eac
Piyush2604 <khattrapiyush80@gmail.com>
2024-08-23 16:06:19 +0530

    working on backend

---
+++ convex/auth.config.ts
@@ -0,0 +1,11 @@
+
+const authConfig = {
+    providers: [
+      {
+        domain: "https://finer-grouse-71.clerk.accounts.dev/",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+  export default authConfig;
---
+++ convex/schema.ts
@@ -0,0 +1,32 @@
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+    podcasts: defineTable({
+        audioStorageId: v.optional( v.id('_storage')),
+        user: v.id('users'),
+        podcastTitle: v.string(),
+        podcastDescription: v.string(),
+        audioUrl: v.optional(v.string()),
+        imageURL: v.optional(v.string()),
+        imageStorageId: v.optional( v.id('_storage')),
+        author: v.string(),
+        authorId: v.string(),
+        authorImageUrl: v.string(),
+        voicePrompt: v.string(),
+        imagePrompt: v.string(),
+        voiceType: v.string(),
+        audioDuration: v.number(),
+        views: v.number(),
+        
+    })
+        .searchIndex('search_author', {searchField: 'author'})
+        .searchIndex('search_title', {searchField: 'podcastTitle'})
+        .searchIndex('search_description', {searchField: 'podcastDescription'}),
+    users: defineTable({
+        email: v.string(),
+        name: v.string(),
+        imageURL: v.string(),
+        clerkId: v.string(),
+    })
+})
